02441818484091886b6ff17cd23fd871638c0ce0
zhengxian.lzx <zhengxian.lzx@alibaba-inc.com>
2020-12-04 10:38:28 +0800

    use avformat delete files

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "libavutil/log.h"
+#include "libavformat/avformat.h"
 
 int main() {
     printf("Hello, World!\n");
     av_log_set_level(AV_LOG_DEBUG);
-    av_log(NULL, AV_LOG_INFO, "hello %s", "ffmpeg");
+    av_log(NULL, AV_LOG_INFO, "hello %s\n", "ffmpeg");
+    int i = avpriv_io_delete("../test.txt");
+    av_log(NULL, AV_LOG_INFO, "hello %d", i);
     return 0;
 }
